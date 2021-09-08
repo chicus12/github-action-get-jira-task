36dc1ab339cfd23a7df619dd7427d24e62552f8e
chicus12 <chicus12@gmail.com>
2021-09-08 15:46:06 -0600

    initial commit

---
+++ .github/pull_request_template.md
@@ -0,0 +1,13 @@
+## Pull Request
+
+#### What's this PR do?
+
+#### Dependencies?
+
+#### How should this be manually tested?
+
+#### Any background context you want to provide?
+
+#### What are the relevant tickets (bugs/features)?
+
+#### Screenshots (if appropriate)
---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const pullRequestBody  = `
+## Pull Request
+
+#### What's this PR do?
+- Se incluye en el sandbox formato de respuesta generico de API
+- Se adapta en el cliente los queries a al tipo de respuesta generico
+- Se pasa el componente de svgToIcon de js a ts
+
+#### Dependencies?
+
+#### How should this be manually tested?
+
+#### Any background context you want to provide?
+
+#### What are the relevant tickets (bugs/features)?
+https://soinlabs.atlassian.net/browse/BOD-271
+:task|esto es una prueba
+:task|esto es otra prueba
+#### Screenshots (if appropriate)
+`
+
+async function run() {
+  try {
+
+    const body = github.context.payload.pull_request.body;
+
+    if (!body) return;
+
+    const attlasianTask = pullRequestBody.split('\n')
+
+    core.setOutput('task', attlasianTask);
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run()
